353263ae0059b036b4c49ddc16490ffcd02eec03
emfvxkoodaaja <etunimis321@gmail.com>
2024-09-22 12:02:02 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,4 @@
+Citizen.CreateThread(function()
+SetWeaponDamageModifier('weapon_stungun_mp', 0.0)
+SetWeaponDamageModifier('weapon_stungun', 0.0)
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+version '0.1'
+
+description 'Etälamauttimen damage poikkeen'
+
+client_scripts {
+	'client.lua'
+}
